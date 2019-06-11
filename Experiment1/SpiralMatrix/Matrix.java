2acea6b59805b10495c723c27c4e76208414f487
Jovian <qianwei_king@163.com>
2019-06-11 22:33:09 +0800

    package SpiralMatrix done

--- Experiment1/SpiralMatrix/Matrix.java
+++ Experiment1/SpiralMatrix/Matrix.java
@@ -1,4 +1,35 @@
 package SpiralMatrix;
 
 public class Matrix {
+  private int row;
+  private int column;
+  private int[][] matrix;
+
+  public Matrix(int row, int column) {
+    this.row = row;
+    this.column = column;
+    this.matrix = new int[row][column];
+  }
+
+  public void setElement(int element, int row, int column) {
+    this.matrix[row][column] = element;
+  }
+
+  public int getRow() {
+    return row;
+  }
+
+  public int getColumn() {
+    return column;
+  }
+
+  public void printMatrix() {
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < column; j++) {
+        System.out.printf("%5d", matrix[i][j]);
+      }
+      System.out.println();
+    }
+    System.out.println();
+  }
 }
--- Experiment1/SpiralMatrix/SpiralMatrix.java
+++ Experiment1/SpiralMatrix/SpiralMatrix.java
@@ -1,7 +1,38 @@
 package SpiralMatrix;
 
-public class SpiralMatrix {
-  public static void main(String[] args) {
+public class SpiralMatrix extends Matrix{
+  public SpiralMatrix(int n) {
+    super(n, n);
+    spiral();
+  }
 
+  private void spiral() {
+    int rowMin, rowMax, colMin, colMax;
+    rowMin = colMin = 0;
+    rowMax = colMax = getRow() - 1; // row equals to column
+    int element = 1;
+    while (element <= getRow() * getColumn()) {
+      for (int j = colMin; j <= colMax; j++) {
+        setElement(element++, rowMin, j);
+      }
+      for (int i = rowMin + 1; i <= rowMax; i++) {
+        setElement(element++, i, colMax);
+      }
+      for (int j = colMax - 1; j >= colMin; j--) {
+        setElement(element++, rowMax, j);
+      }
+      for (int i = rowMax - 1; i > rowMin; i--) {
+        setElement(element++, i, colMin);
+      }
+      rowMin++;
+      rowMax--;
+      colMin++;
+      colMax--;
+    }
+  }
+
+  public static void main(String[] args) {
+    SpiralMatrix matrix = new SpiralMatrix(5);
+    matrix.printMatrix();
   }
 }
